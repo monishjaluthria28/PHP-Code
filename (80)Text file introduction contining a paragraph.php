1da19bb10c3a0116909bf420c0aef9d646a97071
monishjaluthria28 <cousinforever2002@gmail.com>
2025-04-09 10:35:26 +0530

    (80)Text file introduction contining a paragraph.php

--- (80)Text file introduction contining a paragraph.php
+++ (80)Text file introduction contining a paragraph.php
@@ -5,7 +5,7 @@ WAP in php to create a text file name introduction containing a paragraph about 
 $fileName = "introduction.txt";
 
 // Define the content to be written in the file
-$content = "Hello! My name is Aaryan. I live in New Delhi, India, and I enjoy exploring new technologies, solving challenges, and learning new skills. In my free time, I love watching sports, spending time with my family, and traveling to places that inspire creativity.";
+$content = "Hello! My name is Monish Jaluthria. I live in New Delhi, India, and I enjoy exploring new technologies, solving challenges, and learning new skills. In my free time, I love watching sports, spending time with my family, and traveling to places that inspire creativity.";
 
 // Open the file in write mode or create it if it doesn't exist
 $file = fopen($fileName, "w");
